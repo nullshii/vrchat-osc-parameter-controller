6b3d74d97108da0e62d81a2eecb6166d2d471b71
nullshii <60722967+nullshii@users.noreply.github.com>
2026-06-28 11:38:43 +0400

    Added avatar change detection

--- src/osc.rs
+++ src/osc.rs
@@ -1,6 +1,11 @@
-use std::time::Duration;
+use std::{
+    net::SocketAddr,
+    sync::{Arc, Mutex},
+    time::Duration,
+};
 
-use vrchat_osc::VRChatOSC;
+use rosc::OscPacket;
+use vrchat_osc::{VRChatOSC, models::OscRootNode};
 
 const RETRY_COUNT: u8 = 30;
 
@@ -13,12 +18,19 @@ pub async fn run_osc_loop() {
         }
     };
 
-    let cloned_vrchat_osc = vrchat_osc.clone();
+    // Use Arc<Mutex<...>> so both event loops can safely access/mutate the address
+    let address_shared = Arc::new(Mutex::new(None::<SocketAddr>));
+
+    // Clone specifically for the on_connect block
+    let vrchat_osc_for_connect = vrchat_osc.clone();
+    let address_for_connect = address_shared.clone();
 
     vrchat_osc
         .on_connect(move |res| match res {
             vrchat_osc::ServiceType::OscQuery(_name, addr) => {
-                let vrchat_osc = cloned_vrchat_osc.clone();
+                let vrchat_osc_task = vrchat_osc_for_connect.clone();
+                let address_task = address_for_connect.clone();
+
                 tokio::spawn(async move {
                     let mut counter = 0;
                     let params = loop {
@@ -27,7 +39,52 @@ pub async fn run_osc_loop() {
                         }
                         counter += 1;
 
-                        match vrchat_osc
+                        match vrchat_osc_task
+                            .get_parameter_from_addr("/avatar/parameters", addr)
+                            .await
+                        {
+                            Ok(v) => {
+                                if let Ok(mut guard) = address_task.lock() {
+                                    *guard = Some(addr);
+                                }
+
+                                break v;
+                            }
+                            Err(_) => {
+                                tokio::time::sleep(Duration::from_secs(1)).await;
+                            }
+                        }
+                    };
+                    log::info!("Received parameters: {:?}", params);
+                });
+            }
+            _ => {}
+        })
+        .await;
+
+    let root_node = OscRootNode::new().with_avatar();
+    // Clone specifically for the register block
+    let vrchat_osc_for_register = vrchat_osc.clone();
+    let address_for_register = address_shared.clone();
+
+    match vrchat_osc
+        .register("HeartRate-Service", root_node, move |packet| {
+            let current_addr = address_for_register.lock().ok().and_then(|guard| *guard);
+
+            if let OscPacket::Message(msg) = packet
+                && msg.addr == "/avatar/change"
+                && let Some(addr) = current_addr
+            {
+                let vrchat_osc_task = vrchat_osc_for_register.clone();
+                tokio::spawn(async move {
+                    let mut counter = 0;
+                    let params = loop {
+                        if counter == RETRY_COUNT {
+                            panic!("[OSC] Failed to get parameters after {} tries", RETRY_COUNT);
+                        }
+                        counter += 1;
+
+                        match vrchat_osc_task
                             .get_parameter_from_addr("/avatar/parameters", addr)
                             .await
                         {
@@ -40,7 +97,10 @@ pub async fn run_osc_loop() {
                     log::info!("Received parameters: {:?}", params);
                 });
             }
-            _ => {}
         })
-        .await;
+        .await
+    {
+        Ok(_) => log::info!("[OSC] service registered"),
+        Err(e) => log::warn!("[OSC] Failed to register client: {}", e),
+    }
 }
